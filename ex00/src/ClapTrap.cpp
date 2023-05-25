3e1a0b073f0bf92fd20e76be5c35bb7905c1991a
Saladin Afoh <safoh@student.codam.nl>
2023-05-25 17:02:49 +0200

    Fixed Public functions

--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -13,34 +13,52 @@
 #include <ClapTrap.hpp>
 #include <Color.hpp>
 
-// geen health, geen energy points
-
 /* **************************Public_member_functions************************* */
 
 void ClapTrap::attack(const std::string &target)
 {
+	if (this->_hit_points == 0)
+		return this->log_is_dead("attack");
 	if (this->_energy_points == 0)
 		return this->log_no_energy("attack");
 	this->_energy_points--;
-	std::cout << MAG "ClapTrap " << this->_name << " attacks " << target
-			  << " causing " << this->_attack_damage << " points of damage !"
-			  << std::endl
-			  << "New Energy points: " << _energy_points << NC << std::endl;
+	std::cout << MAG << this->_name << " attacks " << target << " causing "
+			  << this->_attack_damage << " points of damage !" << std::endl
+			  << "New Energy points: " << this->_energy_points << NC
+			  << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	_hit_points = (_hit_points < amount) ? 0 : _hit_points - amount;
-	std::cout << YEL "ClapTrap " << this->_name << " took " << amount
+	if (this->_hit_points == 0)
+	{
+		std::cout << YEL << this->_name << " has already died " << std::endl;
+		return;
+	}
+	this->_hit_points = (_hit_points < amount) ? 0 : _hit_points - amount;
+	std::cout << YEL << this->_name << " took " << amount
 			  << " points of damage !" << std::endl
 			  << "New health_total: " << _hit_points << NC << std::endl;
+	if (this->_hit_points == 0)
+		std::cout << RED << this->_name << " has died " << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
+	unsigned int actual_repair_amount;
+	if (this->_hit_points == 0)
+		return this->log_is_dead("repair");
 	if (this->_energy_points == 0)
-		return this->log_no_energy("attack");
-	this->_energy_points += amount;
+		return this->log_no_energy("repair");
+	this->_energy_points--;
+	actual_repair_amount =
+		(this->_hit_points + amount > 10) ? 10 - this->_hit_points : amount;
+	this->_hit_points += actual_repair_amount;
+	std::cout << GRN << this->_name << " repaired itself for "
+			  << actual_repair_amount << std::endl
+			  << "New health_total: " << _hit_points << std::endl
+			  << "Remaining Energy points: " << this->_energy_points << NC
+			  << std::endl;
 }
 
 /* **************************Loggers***************************************** */
